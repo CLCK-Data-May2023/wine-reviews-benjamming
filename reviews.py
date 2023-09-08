792bf14dfa513969a2b5804367b9f05ad5a6303a
Ben Campbell <ben.campbell.w@gmail.com>
2023-09-07 21:05:10 -0400

    adding code for reviews

--- reviews.py
+++ reviews.py
@@ -1,2 +1,29 @@
-# add your code here
+import pandas as pd
+from statistics import mean
+
+INPUT_PATH = "data/winemag-data-130k-v2.csv.zip"
+OUTPUT_PATH = "data/reviews-per-country.csv"
+
+def wrangle(datapath:"path string") -> pd.DataFrame:
+    # import data
+    reviews = pd.read_csv(datapath)
+    # get relevant information
+    out = reviews.groupby("country").points.agg([len, mean])
+    # clean up dataframe/ format it as expected
+    out = out.rename(columns={"len":"count", "mean":"points"})#.reset_index()
+        # .reset_index call is not necessary and without the CSV is smaller :)
+    # round points column
+    out.points = out.points.apply(lambda x: round(x, ndigits=1))
+
+    return out
+
+
+def main():
+    # wrangle that data
+    df = wrangle(INPUT_PATH)
+    # output to CSV file
+    df.to_csv(OUTPUT_PATH)
+
+if __name__ == "__main__":
+    main()
 
